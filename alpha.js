362134c380630d3db15a8a3ac29797facd24a8a6
ChaikalTDR <110763989+ChaikalTDR@users.noreply.github.com>
2023-10-20 23:16:44 +0700

    Update alpha.js

--- alpha.js
+++ alpha.js
@@ -10,7 +10,7 @@ const fs = require('fs')
 const os = require('os')
 const speed = require('performance-now')
 
-if (BOT_TOKEN == 'YOUR_TELEGRAM_BOT_TOKEN') {
+if (BOT_TOKEN == '6550952253:AAGJEV9kNsYQ7xxBVFolS_2CD7-yX67VxQo) {
     return console.log(lang.noToken)
 }
 
